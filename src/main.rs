ddc14f32e69c6ddee11581d4791159aa37d756b3
swpark6 <sewon1234@naver.com>
2020-01-05 22:20:43 +0900

    init rocket

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
-extern crate slack;
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use] extern crate rocket;
+
+#[get("/")] 
+fn index() -> &'static str {
+     "Hello, world!" 
+}
+
+#[get("/add_music")]
+fn add_music() -> &'static str {
+    "ADD_MUSIC_LOGIC"
+}
 
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/", routes![index, add_music])
+        .launch(); 
 }
